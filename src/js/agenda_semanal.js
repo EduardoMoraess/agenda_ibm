f2d135229ad0a903af101a53110c1022a72304ae
Eduardo Moraes <eduzinho2118@gmail.com>
2026-03-01 15:57:54 -0300

    editor da agenda adicionado

--- src/js/agenda_semanal.js
+++ src/js/agenda_semanal.js
@@ -1,5 +1,13 @@
-function alteraAgenda(){
-    const alteracaoAgenda = document.getElementById('enviar-msg');
-    const addAgenda = document.getElementById('add-agenda');
+function alteraAgenda() {
+    const alteracaoAgenda = document.getElementById('add-agenda');
+
+    alteracaoAgenda.addEventListener("click", () => {
+        const numero = '5587981443129';
+        const mensagem = `Olá!, Gostaria de editar a agenda semanal \n nova agenda semanal:`
+
+        const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+        window.open(url, "_blank");
+    })
 }
 alteraAgenda();
