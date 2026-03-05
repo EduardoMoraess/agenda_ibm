46fc7ba27c4ba7ba35fe5d20e3bd6962370720b7
Eduardo Moraes <eduzinho2118@gmail.com>
2026-03-04 22:42:09 -0300

    nova ideia testada e instalada

--- src/js/agenda_semanal.js
+++ src/js/agenda_semanal.js
@@ -1,3 +1,34 @@
+//Validação do formulario da de alteração
+function formularioLogin() {
+    const passwordInput = document.getElementById('password');
+    const userInput = document.getElementById('user');
+    const btnValidar = document.getElementById('validar');
+    const mainContent = document.querySelector('.sumir'); // Seleciona o container oculto
+
+    btnValidar.addEventListener('click', (event) => {
+        event.preventDefault(); // ESSENCIAL: Impede o recarregamento da página
+
+        const usuario = userInput.value;
+        const senha = passwordInput.value;
+
+        // Validação simples
+        if (senha == "2026" && usuario === "adm") {
+            alert('Login realizado com sucesso!');
+
+            // Troca as classes para exibir o conteúdo
+            mainContent.classList.remove('sumir');
+            mainContent.classList.add('mostrar');
+
+            // Opcional: Esconde o formulário de login após o sucesso
+            document.querySelector('.formulario').style.display = 'none';
+        } else {
+            alert('Verifique suas credenciais!');
+        }
+    });
+}
+formularioLogin();
+
+//Para alterar a agenda via WPP
 function alteraAgenda() {
     const alteracaoAgenda = document.getElementById('add-agenda');
 
@@ -8,7 +39,7 @@ function alteraAgenda() {
         const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
 
         window.open(
-            url, 
+            url,
             "popupWhatsapp",
             "width=1000,height=600,top=200,left=100,resizable=yes,scrollbars=yes"
         );
